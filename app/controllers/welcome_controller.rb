0b862702c95c005afd7feb5ef4c9c9e2582a1f10
Nate <primesitemedia@gmail.com>
2014-10-16 14:45:08 +0800

    copy over index & under_construction html & controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-  end
-  def under_construction
+    if params[:admin] == "true"
+      @current_time = Time.now
+      @delivery_time = @current_time + (45*60)
+      render "index"
+      # or
+      # render 'index', locals: {estimatedTime: Time.now + (45*60)}
+    else
+      render "under_construction", layout: false
+    end
   end
 end
