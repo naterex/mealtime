17c8e13fd9185f1e67277529f136c301aa10872b
Nate <primesitemedia@gmail.com>
2014-10-17 14:00:36 +0800

    Added random images to display at bottom of homepage using ruby

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,26 @@
 class WelcomeController < ApplicationController
   def index
+    @current_time = Time.now
+    @delivery_time = @current_time + (45*60)
+    @delivery_time = @delivery_time.localtime.strftime("%I:%M %P")
+
+    @western_item = FoodItem.where(cuisine: "Western").sample
+    @asian_item = FoodItem.where(cuisine: "Asian").sample
+    @adventurous_item = FoodItem.where(cuisine: "Adventurous").sample
+
+    $i = 0
+    $homepage_num_pics = 4
+    @item = []
+    until $i > $homepage_num_pics  do
+      @item[$i] = FoodItem.all.sample
+      $i +=1;
+    end
+
     if params[:admin] == "true"
-      @current_time = Time.now
-      @delivery_time = @current_time + (45*60)
       render "index"
-      # or
-      # render 'index', locals: {estimatedTime: Time.now + (45*60)}
     else
       render "under_construction", layout: false
     end
+
   end
 end
